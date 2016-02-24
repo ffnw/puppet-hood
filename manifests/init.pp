553d06fbc0532239e22cd927dda22cd13c34fc16
Simon Kurka <dev@simon.kurka.cc>
2016-02-24 20:49:39 +0100

    duplicate declaration error resolved

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,13 @@ class hood (
   String $bandwidth = $hood::params::bandwidth,
 ) inherits hood::params {
 
-  require batman
-  require fastd
-  require dhcp
+  class { 'batman': }
+  class { 'fastd': }
+  class { 'dhcp': }
+
+  contain batman
+  contain fastd
+  contain dhcp
 
   create_resources('hood::create', hiera('hood::create', {}))
 
