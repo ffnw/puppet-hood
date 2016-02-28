43b07c1c000549b45e9e01db75c8a58ab5463668
Simon Kurka <dev@simon.kurka.cc>
2016-02-28 12:58:19 +0100

    variable reassignment of fastd

--- manifests/create.pp
+++ manifests/create.pp
@@ -22,13 +22,13 @@ define hood::create (
   dhcp::dhcp { $title:
     interface => "bat-${title}",
     subnet    => $subnet,
-    subnet6    => $subnet6,
+    subnet6   => $subnet6,
   }
 
-  $fastd = $fastd - 1
+  $decreased_fastd = $fastd - 1
   if $fastd > 0 {
 
-    $fastd_instances = range('0', "${fastd}")
+    $fastd_instances = range('0', "${decreased_fastd}")
     $fastd_instances.each | $instance | {
       fastd::instance { $title:
         port             => 10000 + $instance,
