caf0d279c81336ce0e388e7e87507f5742e5b885
Simon Kurka <dev@simon.kurka.cc>
2016-09-07 22:48:50 +0200

    purge gluoncollector and dnsmasq

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,6 +16,22 @@ class hood (
   contain radvd
   contain hopglassserver
 
+  file {
+    default:
+      ensure => absent;
+    '/etc/node-collector.yaml':;
+    '/opt/gluon-collector':
+  }
+
+  service { 'dnsmasq':
+    ensure => stopped,
+    enable => false,
+  }
+
+  package { 'dnsmasq':
+    ensure => purged,
+  }
+
   create_resources('hood::create', hiera('hood::create', {}))
 
 }
